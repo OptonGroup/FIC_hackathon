916edc2ed4226de6bec2cd13ecc5ab3c5e633f14
OptonGroup <maxsimakov123@yandex.ru>
2024-12-01 20:02:35 +0300

    Implement currency rates fetching and display: Update main page to retrieve currency rates and pass them to the template. Modify base_main.html to conditionally display currency rates or an error message if unavailable. Enhance JavaScript to handle currency rate updates more robustly.

--- finvision/views.py
+++ finvision/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from operations.views import get_currency_rates
 
 def main_page(request):
-    return render(request, 'main.html') 
+    # Получаем курсы валют
+    currency_rates = get_currency_rates()
+    
+    context = {
+        'currency_rates': currency_rates
+    }
+    
+    return render(request, 'main.html', context) 
